3d2406d498dbaf82cf2e95b3050bfac71a67dfa3
Diego Giorgini <diegobit@icloud.com>
2026-03-07 12:54:54 +0100

    add privacy policy

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Navigate, Route, Routes } from 'react-router-dom'
 import AppShell from './components/AppShell'
 import DropboxCallback from './routes/DropboxCallback.tsx'
+import Privacy from './routes/Privacy.tsx'
 import Settings from './routes/Settings.tsx'
 import Timeline from './routes/Timeline.tsx'
 
@@ -10,6 +11,7 @@ export default function App() {
       <Route element={<AppShell />}>
         <Route path="/" element={<Timeline />} />
         <Route path="/settings" element={<Settings />} />
+        <Route path="/privacy" element={<Privacy />} />
         <Route path="/auth/dropbox/callback" element={<DropboxCallback />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Route>
