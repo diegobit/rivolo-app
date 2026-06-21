5e518edd6fce45977ed2c867dc6c204cbcd5e2b0
Diego Giorgini <diegobit@icloud.com>
2026-06-21 22:20:14 +0200

    add a small footer in settings with version and about

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
+import { version } from './package.json'
 
 // https://vite.dev/config/
 export default defineConfig({
+  define: {
+    __APP_VERSION__: JSON.stringify(version),
+  },
   server: {
     port: 5174,
   },
